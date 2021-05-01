afe5115e7b13c7df344d6e81c49b153883120f0b
Julián Koroluk <julian.koroluk@outlook.com>
2021-05-01 17:50:08 -0300

    Ejercicio de Profundización 1
    Hecho el ejercicio de profundización número 1

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -15,7 +15,7 @@
 # Ejercicios de práctica con números
 '''
 Enunciado:
-Realice un calculadora, se ingresará por línea de comando dos
+Realice una calculadora, se ingresará por línea de comando dos
 números reales y se deberá calcular todas las operaciones entre ellos:
 A) Suma
 B) Resta
@@ -31,3 +31,40 @@ E) Exponente/Potencia
 
 print('¡Nuestra primera calculadora!')
 # Empezar aquí la resolución del ejercicio
+
+print("Ingrese dos números reales, por favor.")
+
+#Ingreso de la primer variable flotante
+print("Número 1: ")
+numero_1 = float(input())
+
+#Ingreso de la segunda variable flotante
+print("Número 2: ")
+numero_2 = float(input())
+
+#A) Suma
+suma = numero_1 + numero_2
+print("El resultado de la suma entre ", numero_1, " y ", numero_2, " es ", suma)
+
+#B) Resta
+resta_1 = numero_1 - numero_2
+print("El resultado de la resta entre ", numero_1, " y ", numero_2, " es ", resta_1)
+resta_2 = numero_2 - numero_1
+print("El resultado de la resta entre ", numero_2, " y ", numero_1, " es ", resta_2)
+
+#C) Multiplicación
+mult = numero_1 * numero_2
+print("El resultado de la multiplicación entre ", numero_1, " y ", numero_2, " es ", mult)
+
+#D) División
+div_1 = numero_1 / numero_2
+print("El resultado de la división entre ", numero_1, " y ", numero_2, " es ", div_1)
+div_2 = numero_2 / numero_1
+print("El resultado de la división entre ", numero_2, " y ", numero_1, " es ", div_2)
+
+#E) Exponente/Potencia
+
+pot_1 = numero_1 ** numero_2
+print("El resultado de hacer ", numero_1, " elevado a la ", numero_2, " es ", pot_1)
+pot_2 = numero_2 ** numero_1
+print("El resultado de hacer ", numero_2, " elevado a la ", numero_1, " es ", pot_2)
