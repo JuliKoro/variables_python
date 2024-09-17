426b95782d8416117fe56b511158ba2116733f2c
JuliKoro <julian.koroluk@outlook.com>
2024-09-17 12:12:34 -0300

    Update refactor 3.0

--- ejemplos_clase/ejemplo_3.py
+++ ejemplos_clase/ejemplo_3.py
@@ -2,21 +2,25 @@
 # Ejemplos de clase
 
 # Autor: Inove Coding School
-# Version: 2.0
+# Version: 3.0
 
 # Ejemplos capturando información desde la consola
 
-# Comencemos a ingresar datos por medio de la consola
-# Primero mostraremos en pantalla el dato deseado a ingresar
-# y luego esperaremos por el
-print('Ingrese su nombre:')
-nombre = str(input())
+# - Comencemos a ingresar datos por medio de la consola
+# Input nos permite mostrar en pantalla un texto
+# y luego la consola se queda esperado por un dato
+# a ingresar por nosotros
+# - En este caso ingresaremos texto, por lo que
+# transformaremos el dato ingresado por consola a texto --> str
+nombre = str(input('Ingrese su nombre:'))
 print('Nombre ingresado:', nombre)
 
-print('Ingrese cuantos años tiene:')
-edad = int(input())
+# Utilizamos "int" cuando lo que se desea ingresar
+# es un número entero (sin coma)
+edad = int(input('Ingrese cuantos años tiene:'))
 print('Edad ingresada:', edad)
 
-print('Ingrese su altura en metros:')
-altura = float(input())
+# Utilizamos "float" cuando lo que se desea ingresar
+# es un número decimal (con coma)
+altura = float(input('Ingrese su altura en metros:'))
 print('Altura ingresada:', altura)
